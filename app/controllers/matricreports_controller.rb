93356a1aa7590b58baba4ecd3590ca75f52898c1
vongani <vongani@zyelabs.net>
2017-12-06 14:40:00 +0200

    align links

--- app/controllers/matricreports_controller.rb
+++ app/controllers/matricreports_controller.rb
@@ -4,7 +4,7 @@ class MatricreportsController < ApplicationController
   # GET /matricreports
   # GET /matricreports.json
   def index
-    @matricreports = Matricreport.search(params[:search]).paginate(:page => params[:page], :per_page => 500)
+    @matricreports = Matricreport.search(params[:search]).paginate(:page => params[:page], :per_page => 500).order("school_name ASC")
   end
 
   # GET /matricreports/1
